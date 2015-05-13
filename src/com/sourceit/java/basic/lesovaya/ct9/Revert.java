991d7d4c42cbd8b43ed0907f2b8b091e921b485f
AnnaSL <a.lesovaya@gmail.com>
2015-05-13 11:08:39 +0300

    Revert

--- src/com/sourceit/java/basic/lesovaya/ct9/Revert.java
+++ src/com/sourceit/java/basic/lesovaya/ct9/Revert.java
@@ -3,28 +3,34 @@ package com.sourceit.java.basic.lesovaya.ct9;
 public class Revert {
 
 	public static void main(String[] args) {
-		
-		String s = args[0];
-		
+
+		String s;
+		if (args.length > 0) {
+			s = args[0];
+		} else {
+			System.out.println("Array of arguments is empty");
+			return;
+		}
+
 		System.out.println("params: " + s);
-		
+
 		StringBuilder result = new StringBuilder();
-		
+
 		char[] charArray = s.toCharArray();
-		
-		for(int i = s.length() - 1; i>=0; i--){
+
+		for (int i = s.length() - 1; i >= 0; i--) {
 			result.append(charArray[i]);
 		}
-		
-		//T
-		//iT
-		//tiT
+
+		// T
+		// iT
+		// tiT
 		//
-		
-		for(char c: s.toCharArray()){
+
+		for (char c : s.toCharArray()) {
 			result.insert(0, c);
 		}
-		
+
 		System.out.println("result: " + result);
 
 	}
