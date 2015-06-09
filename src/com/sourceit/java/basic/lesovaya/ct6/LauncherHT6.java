4fab4157fbd88d25c7920d2c4f62af71ad0b8beb
User <User@172.30.0.69>
2015-06-09 17:12:24 +0300

    OOP, vehicle

---
+++ src/com/sourceit/java/basic/lesovaya/ct6/LauncherHT6.java
@@ -0,0 +1,12 @@
+package com.sourceit.java.basic.lesovaya.ct6;
+
+import com.souceit.java.basic.lesovaya.ct6.Car;
+
+public class LauncherHT6 {
+
+	public static void main(String[] args) {
+		Car car = new Car();
+		System.out.println("Car " + car.toString() + "was created");
+	}
+
+}
